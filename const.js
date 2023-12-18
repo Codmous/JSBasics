6680567786b5c049bf77c621ee398bdba385106d
Codmous <petrembuga@hotmail.com>
2023-12-18 16:54:43 +0300

    Assigning consta a value from inital undefined state.

--- const.js
+++ const.js
@@ -3,3 +3,10 @@ function example() {
     // z = 40; // Error: Assignment to a constant variable
     console.log(z); // Outputs 30
 }
+
+//You can always declare an undefined value to a const and change it later.
+const myConstVariable = undefined;
+console.log(myConstVariable);
+//UPdated value of myConstVariable initially undefined.
+myConstVariable = 42;
+console.log(myConstVariable);
