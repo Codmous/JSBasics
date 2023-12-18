69c23e3fcd13328e54c04de99692bedddb8901e3
Codmous <petrembuga@hotmail.com>
2023-12-18 17:32:25 +0300

    Comparison Operators.

---
+++ Operators/comparison_Ops/comparison.js
@@ -0,0 +1,9 @@
+var x = 10;
+var y = 5;
+
+console.log(x == y); // Equal to: false
+console.log(x != y); // Not equal to: true
+console.log(x > y);  // Greater than: true
+console.log(x < y);  // Less than: false
+console.log(x >= y); // Greater than or equal to: true
+console.log(x <= y); // Less than or equal to: false
