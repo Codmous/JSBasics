89c858ae9c54a0ec55fe0e46ef7595b5bb776446
Codmous <petrembuga@hotmail.com>
2023-12-18 17:46:34 +0300

    Assignment operators.

---
+++ Operators/assignment_Ops/assignment.js
@@ -0,0 +1,26 @@
+var c = 10;
+var d = 20;
+var e = 5;
+var f = 50;
+var g = 15;
+
+// Addition Assignment
+c += 5; // Equivalent to c = c + 5;
+
+// Subtraction Assignment
+d -= 3; // Equivalent to d = d - 3;
+
+// Multiplication Assignment
+e *= 4; // Equivalent to e = e * 4;
+
+// Division Assignment
+f /= 2; // Equivalent to f = f / 2;
+
+// Modulus Assignment
+g %= 7; // Equivalent to g = g % 7;
+
+console.log(c); // Outputs: 15
+console.log(d); // Outputs: 17
+console.log(e); // Outputs: 20
+console.log(f); // Outputs: 25
+console.log(g); // Outputs: 1
