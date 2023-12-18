a387ada9b3f2e6fc9c2e6a3e2833c2e29fc1394f
Codmous <petrembuga@hotmail.com>
2023-12-18 17:26:37 +0300

    Arithmetic operators.

---
+++ Operators/arithmetics_Ops/arithmetics.js
@@ -0,0 +1,8 @@
+var a = 5;
+var b =  2;
+
+console.log(a + b); // Addition: 7
+console.log(a - b); // Subtraction 3
+console.log(a * b); // Multiplication: 10
+console.log(a / b); // Division: 2.5
+console.log(a % b); // Modulus (remainder): 1
