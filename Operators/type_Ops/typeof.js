38da113c0f43d81d87924580e676aaee220c1fe9
Codmous <petrembuga@hotmail.com>
2023-12-19 19:29:05 +0300

    Type: typeof operators.

---
+++ Operators/type_Ops/typeof.js
@@ -0,0 +1,9 @@
+//USed to get the data type of an object
+
+var num = 7;
+var txt = "Codmous";
+var boo = true;
+
+console.log(typeof num); //returns 'number'
+console.log(typeof txt); //returns 'string'
+console.log(typeof boo); //returns 'boolean'
