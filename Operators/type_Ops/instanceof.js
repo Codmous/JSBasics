5dfe48373022bd3a4a09eb50cee19efc63892927
Codmous <petrembuga@hotmail.com>
2023-12-19 19:33:28 +0300

    Type: instanceof operator.

---
+++ Operators/type_Ops/instanceof.js
@@ -0,0 +1,6 @@
+//Used to check if an obejct is an instance of a particular class or constructor
+var arr = [1, 2, 3];
+
+console.log(arr instanceof Array); //Returns true
+console.log(arr instanceof Object);//Returns true(Arrays are objects)
+console.log(arr instanceof String);//Returns false
