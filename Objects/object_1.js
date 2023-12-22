4f5d21ec92762e81b2c8c4181a3d87109bace8e2
Codmous <petrembugua@hotmail.com>
2023-12-22 23:54:21 +0300

    Minute edits.

--- Objects/object_1.js
+++ Objects/object_1.js
@@ -1,3 +1,3 @@
 const car = {type: "Discovery 4", model:"2017", color:"Darkgrey"};
 
-console.log("This is a " + car.type +"," + car.model + "model")
+console.log("This is a " + car.color + car.type +"," + car.model + "model")
