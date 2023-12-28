88731d071142c8d4b861842741986af1976aa190
Codmous <petrembugua@hotmail.com>
2023-12-28 17:57:27 +0300

    Ternary_Ops revision.

---
+++ Operators/ternary_Ops/ternary_rev.js
@@ -0,0 +1,8 @@
+let nameIs = "Billy";
+
+let corectName = (nameIs=="Billy") ? "Hello there Billy" : "And who might you be?";
+
+alert(corectName);
+
+//IF the name is Billy, first message is sent. Else, second one.
+//This is short hand for if-else.
